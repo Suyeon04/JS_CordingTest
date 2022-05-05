0dd832cd5582e8a843325389eb88a6befecfe646
Suyeon04 <s2026@e-mirim.hs.kr>
2022-05-05 23:02:36 +0900

    3진법뒤집기

---
+++ 3진법뒤집기.js
@@ -0,0 +1,23 @@
+function solution(n) {
+    var answer = 0;
+    let x = 1;
+    while(n!=0){
+        answer += n%3*x;
+        n=parseInt(n/3);
+        x*=10;
+    }
+    let answer1 = 0;
+    let y = 1;
+    for(let i = x/10; i!=0; i=parseInt(i/10)){
+        answer1 += parseInt(answer/i)*y;
+        answer-=parseInt(answer/i)*i;
+        y*=10;
+    }answer = 0;
+    x = 1;
+     while(answer1!=0){
+         answer+=answer1%10*x;
+         x*=3;
+         answer1=parseInt(answer1/10);
+    }
+    return answer;
+}
